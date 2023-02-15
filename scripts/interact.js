97b8cca1c7e71a97ae77130214a81f7ddd3ab594
Kunal Barua <kunalbarua@Kunals-MacBook-Air.local>
2023-02-15 11:55:56 +0530

    to setup operator

--- scripts/interact.js
+++ scripts/interact.js
@@ -1,4 +1,6 @@
 const API_KEY = process.env.API_KEY;
+
+// staking provider Private Key
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 const T_token = require("./T.json")
@@ -12,13 +14,13 @@ const signer = new ethers.Wallet(PRIVATE_KEY, infuraProvider);
 
 //adresses
 const stakingProvider = "0xd5b0a91e1Ab74F0c20b00BD070ef333B36F5B06d";
-const beneficiary = "0xd5b0a91e1Ab74F0c20b00BD070ef333B36F5B06d";
-const authorizer = "0xd5b0a91e1Ab74F0c20b00BD070ef333B36F5B06d";
+const beneficiary = stakingProvider;
+const authorizer = stakingProvider;
 const operator = "0x875DAdf51Ca83f7E4b4186a78460A7B2d6439f6c";
 
-const stakingProvider1 = "0xA65Bf47514e3e6CE8E461315cEd6Ae1eaab2F0D0";
-const beneficiary1 = "0xA65Bf47514e3e6CE8E461315cEd6Ae1eaab2F0D0";
-const authorizer1 = "0xA65Bf47514e3e6CE8E461315cEd6Ae1eaab2F0D0";
+// const stakingProvider1 = "0xA65Bf47514e3e6CE8E461315cEd6Ae1eaab2F0D0";
+// const beneficiary1 = "0xA65Bf47514e3e6CE8E461315cEd6Ae1eaab2F0D0";
+// const authorizer1 = "0xA65Bf47514e3e6CE8E461315cEd6Ae1eaab2F0D0";
 
 const overrides = { gasLimit: 2000000 };
 
@@ -29,27 +31,28 @@ const staking = new ethers.Contract(T_Staking.address,T_Staking.abi, signer);
 const tbtc = new ethers.Contract(ECDSA.address,ECDSA.abi, signer);
 
 
-staking.on("AuthorizationIncreased", (stakingProvider, application, fromAmount, authorized) => {
-    console.log("AuthorizationIncreased event: ", {
-        stakingProvider,
-        application,
-        fromAmount: fromAmount.toString(),
-        authorized: authorized.toString()
-    });
-})
+// staking.on("AuthorizationIncreased", (stakingProvider, application, fromAmount, authorized) => {
+//     console.log("AuthorizationIncreased event: ", {
+//         stakingProvider,
+//         application,
+//         fromAmount: fromAmount.toString(),
+//         authorized: authorized.toString()
+//     });
+// })
 
 
 async function main() {
     console.log("Starting");
-    // // stake
-    // const stakeAmount = 1000000;
-    // let stakeAmt = ethers.utils.parseUnits(stakeAmount.toString(), "ether");
-    // const step0 = await T.approve(staking.address,stakeAmt,overrides);
-    // await step0.wait();
-    // console.log("++++++++++++++step0++++++++++++");
-    // const step1 = await staking.stake(stakingProvider,beneficiary,authorizer,stakeAmt,overrides);
-    // await step1.wait();
-    // console.log("++++++++++++++step1++++++++++++");
+    // stake
+    const stakeAmount = 100000;
+    let stakeAmt = ethers.utils.parseUnits(stakeAmount.toString(), "ether");
+    
+    const step0 = await T.approve(staking.address,stakeAmt,overrides);
+    await step0.wait();
+    console.log("++++++++++++++step0++++++++++++");
+    const step1 = await staking.stake(stakingProvider,beneficiary,authorizer,stakeAmt,overrides);
+    await step1.wait();
+    console.log("++++++++++++++step1++++++++++++");
 
     // //topUp
     // const topUpAmount = 1000000;
@@ -70,18 +73,18 @@ async function main() {
         console.error(error);
     }
     console.log("++++++++++++++step2a++++++++++++");
-    // try{const step2b = await staking.increaseAuthorization(stakingProvider1,Beacon.address,authAmt,overrides);
-    // await step2b.wait();}
-    // catch(error){
-    //     console.error(error);
-    // }
-    // console.log("++++++++++++++step2b++++++++++++");
-    // try{const step3a = await randomBeacon.registerOperator(operator,overrides);
-    // await step3a.wait();}
-    // catch(error){
-    //     console.error(error);
-    // }
-    // console.log("++++++++++++++step3a++++++++++++")
+    try{const step2b = await staking.increaseAuthorization(stakingProvider1,Beacon.address,authAmt,overrides);
+    await step2b.wait();}
+    catch(error){
+        console.error(error);
+    }
+    console.log("++++++++++++++step2b++++++++++++");
+    try{const step3a = await randomBeacon.registerOperator(operator,overrides);
+    await step3a.wait();}
+    catch(error){
+        console.error(error);
+    }
+    console.log("++++++++++++++step3a++++++++++++")
     try{const step3b = await tbtc.registerOperator(operator,overrides);
     await step3b.wait();
     console.log(step3b);}
@@ -92,3 +95,5 @@ async function main() {
 }
 
 main();
+
+// To run: npx hardhat run scripts/interact.js --network goerli 
